60e59f752f03385b527234a34d6f188ca34ff77a
Chompoo <knwchbr@MacBook-Pro-khxng-knwchbr.local>
2020-01-12 22:43:41 +0700

    home work

--- xhashh.py
+++ xhashh.py
@@ -7,9 +7,11 @@ def xhash():
 
         y_dict={'TAN':'00001','MAC':'00002','NUT':'00003','PAK':'00004','FAS':'00005','ZAX':'00006','NEX':'00007','PEN':'00008','PHU':'00009','CHO':'00010'}
         sum = 0
+        result = 0
         for i in range(len(name)):
             sum = sum + ord(name[i]) 
-        print(sum % 10)
+            result = (sum % 10)
+        print(result)
         print(y_dict.get(name))
         more = input('Add more (y): ')
 xhash()
